d8002800fd895fdb6a2fe865f2b81028c76537b5
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-02-09 00:25:49 +0100

    add hover for the buttun

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,6 +1,7 @@
 
 import { MdOutlineFavoriteBorder } from "react-icons/md";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 
 
@@ -11,17 +12,28 @@ setIsFavoriteIcon(!IsFavoriteIcon)
 }
   return (
  
-    <div className="border border-amber-50 rounded-lg w-40 h-72  p-2 shadow-md shadow-blue-300">
+    <motion.div    whileHover={{
+                         scale: 0.98,
+                         backgroundColor: " rgba(113, 125, 113, 0.8)",
+                         color: "#fff",
+                         boxShadow: "0px 0px 15px rgba(0, 191, 255, 0.8)", }}
+    className="border border-amber-50 rounded-lg w-40 h-72  p-2 shadow-md shadow-blue-300">
       <img src={poster_path} alt="" className=" w-full h-40 object-contain" />
       <p className="text-white mt-5 text-center">
   {original_title.split(' ').slice(0, 2).join(' ')}
 </p>
       <div className="flex justify-between">
         <p className="text-white mt-1 text-sm">Price:{popularity}$ </p> 
-       <p className={`text-md cursor-pointer mt-2 ${IsFavoriteIcon ? "text-rose-700"   :"text-white" }`} onClick={FavoriteIcone}><MdOutlineFavoriteBorder /></p>
+       <p className={`text-md hover:text-rose-700 cursor-pointer mt-2 ${IsFavoriteIcon ? "text-rose-700"   :"text-white" }`} onClick={FavoriteIcone}><MdOutlineFavoriteBorder /></p>
       </div>
-    <button className="mt-3 text-white border-1 rounded-2xl cursor-pointer w-30 h-8 ml-2.5">Add To Card</button>
-    </div>
+    <motion.button 
+whileHover={{scale: 1.04,
+  backgroundColor: " rgba(113, 125, 113, 0.8)",
+ color: "#fff",
+boxShadow: "0px 0px 15px rgba(0, 191, 255, 0.8)",
+ }}
+    className="mt-3 text-white border-1 rounded-2xl cursor-pointer w-30 h-8 ml-2.5">Add To Card</motion.button>
+    </motion.div>
   );
 }
 
