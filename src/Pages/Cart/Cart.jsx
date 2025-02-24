ef03bf614fcbf19c39e068f8658dd30b3f3cdc06
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-02-24 10:58:55 +0100

    total price

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,10 +1,27 @@
 import CartItem from "../../Components/ShareComponents/CartItem/CartItem";
 import { AppContext } from "../../Context/AppContextProvider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { MovieApi } from "../../Services/ProductApi";
 
 function Cart() {
   const { CartItems } = useContext(AppContext);
+  const[products,setProducts]=useState([])
+  
+  useEffect( ()=>{
+    MovieApi.then( (respond)=>{
+      let data=respond.data
+      setProducts(data)
+    })
+    .catch( (error)=>(console.log("error",error)))
+  },[])
 
+
+  let totalPrice=CartItems.reduce( (total,item)=>{
+    let selectedProduct=products.find((_item)=>(_item.id==item.id))
+    return total + ((selectedProduct?.price ?? 0)* item.qty)
+  },0)
+
+ 
   return (
     <div>
       {CartItems.length > 0 ? (
@@ -12,6 +29,7 @@ function Cart() {
       ) : (
         <p className="text-white flex justify-center mt-20 text-3xl ">No Item In Cart</p>
       )}
+      <p>Total Price: {totalPrice}</p>
     </div>
   );
 }
