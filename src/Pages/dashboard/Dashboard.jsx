b359b8cc566fdd2477f471726a9de99c7b4fb286
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-03-03 13:53:56 +0100

    dashboard

--- src/Pages/dashboard/Dashboard.jsx
+++ src/Pages/dashboard/Dashboard.jsx
@@ -2,32 +2,75 @@ import Buttom from "../../Components/CoreComponents/Button/Button";
 import Textarea from "../../Components/CoreComponents/textarea/textarea";
 import Input from "../../Components/CoreComponents/input/input";
 import Wrapper from "../../Components/ShareComponents/Wrapper/Wrapper";
+import { useState } from "react";
 
 function Dashboard() {
-    
+  const [newProduct, setNewProduct] = useState({
+    popularity: "",
+    backdrop_path: "",
+    original_title: "",
+    profile_path: "",
+    Description: "",
+  });
+
+  const handelChange = (e) => {
+    const { name, value } = e.target;
+    setNewProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value, 
+    }));
+    console.log({ [name]: value }); 
+  };
+
+  console.log(newProduct); 
+
   return (
     <div>
       <Wrapper>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4 text-amber-50"> 
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4 text-amber-50">
           <div>
             <label className="block mb-1">Price:</label>
-            <Input className="w-60" type="text" name="Price" />
+            <Input
+              onChange={handelChange} 
+              className="w-60"
+              type="text"
+              name="popularity" 
+            />
           </div>
           <div>
             <label className="block mb-1">Title:</label>
-            <Input className="w-60" type="text" name="original_title" />
+            <Input
+              onChange={handelChange}
+              className="w-60"
+              type="text"
+              name="original_title"
+            />
           </div>
           <div>
             <label className="block mb-1">Background:</label>
-            <Input className="w-60" type="text" name="backdrop_path" />
+            <Input
+              onChange={handelChange}
+              className="w-60"
+              type="text"
+              name="backdrop_path"
+            />
           </div>
           <div>
             <label className="block mb-1 w-full">Cast photo:</label>
-            <Input className="w-60" type="text" name="cast.profile_path" />
+            <Input
+              onChange={handelChange}
+              className="w-60"
+              type="text"
+              name="profile_path" // Changed to match state key
+            />
           </div>
           <div>
             <label className="block mb-1">Description:</label>
-            <Textarea className="w-full h-30" type="text" name="Description" />
+            <Textarea
+              onChange={handelChange}
+              className="w-full h-30"
+              name="Description"
+            />
           </div>
         </div>
         <Buttom>Submit</Buttom>
@@ -37,4 +80,5 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
 
